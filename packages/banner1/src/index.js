3d2cf098bee05da7437c8e375bb9ace1f7e9cd3c
VinayMavi <vinaymavi@gmail.com>
2018-05-15 12:00:53 +0530

    react added as dev dependency.

---
+++ packages/banner1/src/index.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class Banner extends Component{
+    constructor(props){
+        super(props);
+    }
+    
+    render(){return "hello"}
+}
+
+
+export default Banner;
